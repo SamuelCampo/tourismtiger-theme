7b654b09fabb91f1984610e95a518a85607c8740
Dzvonkevich <etrange@web405.default-host.net>
2017-07-05 18:44:26 +0300

    include js via defer

--- inc/common/enqueue.php
+++ inc/common/enqueue.php
@@ -21,6 +21,25 @@ function wordpress_kit_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'wordpress_kit_scripts' );
 
+
+/**
+ * Defer attribute to scripts
+ */
+function add_defer_attribute($tag, $handle) {
+
+   // add script handles to the array below
+   $scripts_to_defer = array( 'wp-embed', 'tourismtiger-js', 'googme-maps-js' );
+   
+   foreach($scripts_to_defer as $defer_script) {
+      if ($defer_script === $handle) {
+         return str_replace(' src', ' defer="defer" src', $tag);
+      }
+   }
+   return $tag;
+}
+add_filter('script_loader_tag', 'add_defer_attribute', 10, 2);
+
+
 /**
  * Include AJAX
  */
