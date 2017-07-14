e6b0a73db07e480b843cb7e2e0920f1cb2397511
Dzvonkevich <saveliy@etrange.com.ua>
2017-07-14 13:38:28 +0300

    init column local fields

--- template-parts/row-content.php
+++ template-parts/row-content.php
@@ -8,20 +8,30 @@
  * @author  tourismtiger
  */
 
-
-// Common 
-$attrs                = array();
-$style                = array();
-$classes              = array();
-$classes[]            = 'row--column';
-
-// Compile classes and attributes
-$attrs[]              = 'style="' . generate_classlist( $style ) . '"';
-$classes              = generate_classlist( $classes );
-$attrs                = generate_classlist( $attrs );
-
-
 /**
- * Get section view
+ * Check whether there are any columns.
+ * Loop them.
  */
-include THEME_VIEWS . 'core/column.php';
+if ( have_rows( 'columns' ) ) :
+	while ( have_rows( 'columns' ) ) :
+		the_row();
+
+		// Common 
+		$attrs                = array();
+		$style                = array();
+		$classes              = array();
+		$classes[]            = 'row--column';
+
+		// Compile classes and attributes
+		$attrs[]              = 'style="' . generate_classlist( $style ) . '"';
+		$classes              = generate_classlist( $classes );
+		$attrs                = generate_classlist( $attrs );
+
+
+		/**
+		 * Get section view
+		 */
+		include THEME_VIEWS . 'core/column.php';
+
+	endwhile;
+endif;
