eb53fb70cc8fb954711cd48dc3362df293bec1be
Dzvonkevich <dzvonkevich@gmail.com>
2017-08-08 17:57:41 +0300

    optimization improvements

--- sidebar-product.php
+++ sidebar-product.php
@@ -8,35 +8,37 @@
  */
 
 if ( have_rows( 'widgets' ) ) :
-	?>
-
-	<div class="sidebar">
-
-		<?php
-		/**
-		 * Loop widgets
-		 */
-		while ( have_rows( 'widgets' ) ) :
-			$the_widget = the_row();
-			$layout     = get_row_layout();
-			?>
-				
-			<div class="sidebar__widget card-wrapper wysiwyg">
-
-				<?php
-				/**
-				 * Widget model
-				 */
-				get_template_part( "template-parts/{$layout}" );
-				?>
-
-			</div>
-
-			<?php
-		endwhile;
+	if ( ! wp_is_mobile() ) :
 		?>
 
-	</div>
+		<div class="sidebar">
 
-	<?php
+			<?php
+			/**
+			 * Loop widgets
+			 */
+			while ( have_rows( 'widgets' ) ) :
+				$the_widget = the_row();
+				$layout     = get_row_layout();
+				?>
+					
+				<div class="sidebar__widget card-wrapper wysiwyg">
+
+					<?php
+					/**
+					 * Widget model
+					 */
+					get_template_part( "template-parts/{$layout}" );
+					?>
+
+				</div>
+
+				<?php
+			endwhile;
+			?>
+
+		</div>
+
+		<?php
+	endif;
 endif;
